db513112a4976a0ecd66f13864ffb2799b12f6b3
himel08 <himelshahadat08@gmail.com>
2023-11-17 23:52:59 +0600

    Folder Change

--- sellersignup.php
+++ sellersignup.php
@@ -31,7 +31,7 @@ if(isset($_GET['signup']))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="sellerStyle.css">
+    <link rel="stylesheet" href="css/sellerStyle.css">
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome.min.css">
 
--- upproduct.php
+++ upproduct.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="loginstyle.css">
+    <link rel="stylesheet" href="css/loginstyle.css">
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome.min.cs">
     <title>Document</title>
